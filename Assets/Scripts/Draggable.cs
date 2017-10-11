aee9af4c68e83889678bd26632aac38634062244
Matheus Moura <matheus.moura@prodeaf.net>
2017-10-10 23:32:05 -0300

    style: Comentarios removidos em Draggable

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -7,14 +7,12 @@ public class Draggable : MonoBehaviour {
 
 	public int damping = 7;
 	private bool dragging;
-	
-	// Update is called once per frame
+
 	void Update () {
 		if (dragging) {
 			Vector2 targetPosition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
 			targetPosition=Camera.main.ScreenToWorldPoint (targetPosition);
 			transform.position = Vector2.Lerp(transform.position,targetPosition,damping*Time.deltaTime);
-			//transform.position=targetPosition;
 		}
 	}
 
