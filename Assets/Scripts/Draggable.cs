8e16c28d40034fe016852d99f1db686580e79db7
Matheus Moura <matheus.moura@prodeaf.net>
2017-10-10 23:41:32 -0300

    feat: Efeito RayTrace adicionado em Draggable

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -3,25 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(LineRenderer))]
 public class Draggable : MonoBehaviour {
 
 	public int damping = 7;
 	private bool dragging;
+	private LineRenderer lineRenderer;
+
+	void Start(){
+		lineRenderer = GetComponent<LineRenderer> ();
+	}
 
 	void Update () {
 		if (dragging) {
+			//movement
 			Vector2 targetPosition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
 			targetPosition=Camera.main.ScreenToWorldPoint (targetPosition);
 			transform.position = Vector2.Lerp(transform.position,targetPosition,damping*Time.deltaTime);
+			//line effect
+			lineRenderer.SetPositions (new Vector3[]{ transform.position, targetPosition });
 		}
 	}
 
 	void OnMouseDown(){
 		dragging = true;
+		lineRenderer.enabled = true;
 	}
 
 	void OnMouseUp(){
 		dragging = false;
+		lineRenderer.enabled = false;
 	}
 		
 }
