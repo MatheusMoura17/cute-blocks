4d2f082a5c870b1bc73c2af289c332ea8b7f4737
Matheus Moura <matheus.moura@prodeaf.net>
2017-10-30 17:04:52 -0300

    feat: Saquinho de pipoca criado

---
+++ Assets/Scripts/Shootable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shootable : MonoBehaviour {
+
+	public Transform spawnPoint;
+	public GameObject shootPrefab;
+	public float impulse=400;
+	public float time=10;
+	public bool byTime;
+
+	void Start(){
+		if (byTime)
+			Invoke ("Shoot", time);
+	}
+
+	public void Shoot(){
+		GameObject gm=Instantiate (shootPrefab, spawnPoint.position, Quaternion.identity);
+		Vector2 direction = transform.up+Vector3.left*(Random.Range (-1.0f, 1.0f));
+		gm.GetComponent<Rigidbody2D> ().AddForce (direction*impulse,ForceMode2D.Force);
+		Invoke ("Shoot", time);
+	}
+}
