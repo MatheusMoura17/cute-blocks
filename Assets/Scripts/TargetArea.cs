b9783c3e14ed9913c626e1045fa09334161779fb
Matheus Moura <matheus.moura@prodeaf.net>
2017-11-13 20:35:11 -0300

    feat: TargetArea implementado

---
+++ Assets/Scripts/TargetArea.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetArea : MonoBehaviour {
+
+	public string itemTag="Item";
+	public List<GameObject> linkedItems;
+
+	// Use this for initialization
+	void Start () {
+		linkedItems = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if(collider.gameObject.CompareTag(itemTag) && !linkedItems.Contains (collider.gameObject))
+			linkedItems.Add (collider.gameObject);
+	}
+
+	void OnTriggerExit2D(Collider2D collider){
+		if(collider.gameObject.CompareTag(itemTag) && linkedItems.Contains (collider.gameObject))
+			linkedItems.Remove (collider.gameObject);
+	}
+}
