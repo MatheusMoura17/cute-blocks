852b377b8d42ae270e36383778285a9e902976c8
Matheus Moura <matheus.moura@prodeaf.net>
2017-11-14 17:33:48 -0300

    fix: Sistema de seleção de leveis fixado

--- Assets/Scripts/SelectLevelManager.cs
+++ Assets/Scripts/SelectLevelManager.cs
@@ -2,26 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SelectLevelManager : MonoBehaviour {
 
+	public Button[] levelButtons;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		PlayerPrefs.SetInt ("level-unlocked1", 1);
+		for (int i = 0; i < levelButtons.Length; i++) {
+			levelButtons[i].interactable=PlayerPrefs.GetInt ("level-unlocked" + (i+1)) == 1;
+		}
 	}
 
     public void Back()
     {
-        SceneManager.LoadScene(SceneManager.sceneCount - 1);
+        SceneManager.LoadScene("Main Menu");
     }
 
-    public void ChangeLevel(string nameLevel)
-    {
-        SceneManager.LoadScene(nameLevel);
-    }
+	public void LoadLevel(int levelNumber){
+		SceneManager.LoadScene("Level"+levelNumber);
+	}
 }
